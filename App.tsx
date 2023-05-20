30ce0c8098dad99a1234fe5c2f96017060410473
Caveira <brunoanderson.macedorosendo@gmail.com>
2023-05-20 12:17:25 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View >
+      <Text className='bg-slate-700'>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
